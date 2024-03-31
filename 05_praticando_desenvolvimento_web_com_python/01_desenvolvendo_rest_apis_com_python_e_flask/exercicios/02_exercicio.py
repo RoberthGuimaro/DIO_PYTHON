85ad4c805fbdfc9a3b3d5c88620ff4dd4c0b75d8
Roberth Guimaro <guimaroroberth@gmail.com>
2024-03-31 09:35:57 -0400

    exercicio_2_finalizado

--- 05_praticando_desenvolvimento_web_com_python/01_desenvolvendo_rest_apis_com_python_e_flask/exercicios/02_exercicio.py
+++ 05_praticando_desenvolvimento_web_com_python/01_desenvolvendo_rest_apis_com_python_e_flask/exercicios/02_exercicio.py
@@ -15,3 +15,79 @@
     - Incluir validação no app-restful para verificar se habilidades
     informadas existem na lista habilidades.
 """
+
+from flask import Flask
+from flask import request
+from flask_restful import Resource
+from flask_restful import Api
+import json
+from habilidades import Habilidades
+from habilidades import SubstituiHabilidade
+
+app = Flask(__name__)
+
+api = Api(app)
+
+desenvolvedores = [
+    {
+        'id':'0',
+        'nome':'Roberth',
+        'habilidades': ['Python', 'Flask', 'Redes']
+    },
+    {
+        'id':'1',
+        'nome':'Larissa',
+        'habilidades':['Bolo', 'Lasanha']
+    }
+]
+
+
+# Devolve um desenvolvedor pelo ID, também altera e deleta um desenvolvedor
+class Desenvolvedor(Resource):
+    def get(self, id):
+        try:
+            response = desenvolvedores[id]
+        
+        except IndexError:
+            mensagem = f'Desenvolvedor de ID {id} não existe'
+            response = {'status': 'error', 'mensagem': mensagem}
+
+        except Exception:
+            mensagem = 'Erro desconhecido. Procure o administrador da API'
+            response = {'status': 'error', 'mensagem': mensagem}
+        return response
+    
+    def put(self, id):
+        dados = request.json
+        desenvolvedores[id] = dados
+        return dados
+    
+    def delete(self, id):
+        desenvolvedores.pop(id)
+        return {'status':'sucesso', 'mensagem':"registro excluido"}
+
+
+# Lista todos os desenvolvedores e permite registrar um novo desenvolvedor
+class ListaDesenvolvedores(Resource):
+    def get(self):
+        return desenvolvedores
+
+    def post(self):
+        dados = request.json
+        posicao = len(desenvolvedores)
+        dados['id'] = posicao
+        desenvolvedores.append(dados)
+        return desenvolvedores[posicao]
+
+
+api.add_resource(Desenvolvedor, '/dev/<int:id>')
+
+api.add_resource(ListaDesenvolvedores, '/dev')
+
+api.add_resource(Habilidades, '/habilidades')
+
+api.add_resource(SubstituiHabilidade, '/habilidades/<int:id>')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- 05_praticando_desenvolvimento_web_com_python/01_desenvolvendo_rest_apis_com_python_e_flask/exercicios/habilidades.py
+++ 05_praticando_desenvolvimento_web_com_python/01_desenvolvendo_rest_apis_com_python_e_flask/exercicios/habilidades.py
@@ -1,7 +1,69 @@
-from flask_restful import Resource
+"""
+    Exercício para praticar
 
+    - Acrescentar na API de habilidade (módulo habilidades) os métodos
+    PUT, POST e DELETE.
+
+    - O método POST deverá inserir uma habilidade nova na lista.
+
+    - O método PUT a partir de um ID (identificador da posição)
+    deverá alterar o nome da habilidade que está naquela posição.
+
+    - O método DELETE deverá deletar uma habilidade que esteja na
+    posição informada na requisição.
+
+    - Incluir validação no app-restful para verificar se habilidades
+    informadas existem na lista habilidades.
+"""
+
+from flask_restful import Resource
+from flask import request
+
+# Lista de habilidades
 lista_habilidades = ['Python', 'Java', 'Flask', 'Django']
 
+# Class Habilidades que fara as modificacoes e retornos da lista_habiilidades
 class Habilidades(Resource):
+    # Retorna a lista de habilidades
     def get(self):
         return lista_habilidades
+    
+    # Insere uma nova habilidade na lista
+    def post(self):
+        dados = request.json
+        # Checa se habilidade ja existe na lista
+        if dados in lista_habilidades:
+            return {'message':'Habilidade ja existente'}
+        
+        else:
+            # Inclui habilidade na lista habilidades
+            lista_habilidades.append(dados)
+            return dados
+    
+class SubstituiHabilidade(Resource):
+    
+    def put(self, id):
+        dados = request.json
+        # Checa se o tamanho do id e maior que zero e menor que o tamanho da lista
+        # substitui a habilidade antiga pela nova
+        if 0 <= id < len(lista_habilidades):
+            habilidade_antiga = lista_habilidades[id]
+            lista_habilidades[id] = dados
+            return {'habilidade antiga':habilidade_antiga, 'habilidade nova': lista_habilidades[id]}
+        
+        # Caso o id seja maior que a lista retorna um erro
+        else:
+            return {'message':'ID maior que o numero de itens na lista'}
+
+    def delete(self, id):
+        # Checa se o tamanho do id e maior que zero e menor que o tamanho da lista
+        # Apaga a habilidade com id especificado
+        if 0 <= id < len(lista_habilidades):
+            habilidade = lista_habilidades[id]
+            lista_habilidades.pop(id)
+            message = f"Habilidade {habilidade} foi apagada"
+            return {'messsage':message}
+        
+        # Caso o id seja maior que a lista retorna um erro
+        else:
+            return {'message':'ID maior que o numero de itens na lista'}
